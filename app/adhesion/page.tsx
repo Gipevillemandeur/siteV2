fa710dada000293fffe17df24fc1cf1985a377a0
Gipevillemandeur <contact@gipevillemandeur.com>
2026-09-16 21:22:12 +0200

    Update page.tsx

--- app/adhesion/page.tsx
+++ app/adhesion/page.tsx
@@ -48,7 +48,7 @@ export default function AdhesionPage() {
           </h2>
           <iframe
             id="haWidget"
-            src="https://www.helloasso.com/associations/groupement-independant-de-parents-d-eleves-du-college-de-villemandeur/adhesions/adhesion-gipe-villemandeur/widget-bouton"
+            src="https://www.helloasso.com/associations/groupement-independant-de-parents-d-eleves-du-college-de-villemandeur/adhesions/adhesion-gipe-villemandeur/widget-vignette"
             style={{ width: '100%', height: '750px', border: 'none' }}
           ></iframe>
         </div>
